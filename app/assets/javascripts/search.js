6ca236d3b950d55fd6e1edb1748edb840a13f3ba
umi2019 <uamanuma@gmail.com>
2019-02-22 17:42:53 +0900

    Fix search function

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,10 +1,15 @@
 $(function(){
   $(".search__form__input").on('keyup', function(event) {
-    Rails.fire($(".search__form")[0], "submit");
+    if ($(this).val().length){
+      Rails.fire($(".search__form")[0], "submit");
+    }
+    else{
+      $(".result").empty();
+    }
   });
 
   $(".search__form").on('ajax:success', function(event) {
       console.log('success');
       $(".result").html(event.detail[2].response);
-    });
+  });
 });
